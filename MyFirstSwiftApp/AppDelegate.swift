fc13cca50547930243e1cdb6470dcba9db5d2bb4
ats <yasuda.atusi@gmail.com>
2014-08-02 17:40:43 +0900

    Finish tutorial

--- MyFirstSwiftApp/AppDelegate.swift
+++ MyFirstSwiftApp/AppDelegate.swift
@@ -11,7 +11,9 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
                             
     @IBOutlet weak var window: NSWindow!
-
+    @IBOutlet weak var txtFirstName: NSTextField!
+    @IBOutlet weak var txtLastName: NSTextField!
+    @IBOutlet weak var txtFullName: NSTextField!
 
     func applicationDidFinishLaunching(aNotification: NSNotification?) {
         // Insert code here to initialize your application
@@ -22,5 +24,15 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
 
 
+    @IBAction func btnConcatenate(sender: AnyObject) {
+        txtFullName.stringValue = txtFirstName.stringValue + " " + txtLastName.stringValue
+        
+        let myPopup:NSAlert = NSAlert()
+        
+        myPopup.messageText = "Success!"
+        myPopup.informativeText = "Results are shown in the Full Name field. And the result is: \(txtFullName.stringValue)"
+        
+        myPopup.runModal()
+    }
 }
 
