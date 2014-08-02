3f55bda12c0678739f545e2eacd25e2adf9aa3cd
ats <yasuda.atusi@gmail.com>
2014-08-02 17:15:19 +0900

    Initial Commit

---
+++ MyFirstSwiftApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MyFirstSwiftApp
+//
+//  Created by ats on 2014/08/02.
+//
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MyFirstSwiftApp/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  MyFirstSwiftApp
+//
+//  Created by ats on 2014/08/02.
+//
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
